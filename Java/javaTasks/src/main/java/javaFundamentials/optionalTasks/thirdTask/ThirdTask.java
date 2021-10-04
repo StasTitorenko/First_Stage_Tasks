680a0d0e3466b50fa01cbfc20c3652524733123b
Stas Titorenko <fanta2000@tut.by>
2021-10-04 09:02:10 +0300

    1.add "space" between operators

--- Java/javaTasks/src/main/java/javaFundamentials/optionalTasks/thirdTask/ThirdTask.java
+++ Java/javaTasks/src/main/java/javaFundamentials/optionalTasks/thirdTask/ThirdTask.java
@@ -12,18 +12,18 @@ public class ThirdTask {
         System.out.print("Enter the number of items you want to enter: ");
         int itemNumber = input.nextInt();
         String[] itemArray = new String[itemNumber];
-        int allLength=0;
-        for (int i=0;i<itemNumber;i++) {
-            System.out.print("Enter "+(i+1)+" item:");
-            itemArray[i]=input.next();
-            allLength+=itemArray[i].length();
+        int allLength = 0;
+        for (int i = 0; i < itemNumber; i++) {
+            System.out.print("Enter " + (i + 1) + " item:");
+            itemArray[i] = input.next();
+            allLength += itemArray[i].length();
         }
         input.close();
         int mediumLength = allLength/itemNumber;
-        System.out.println("Medium length="+mediumLength);
-        for (int i=0;i<itemNumber;i++){
-            if (itemArray[i].length()>mediumLength)
-                System.out.println("Number "+itemArray[i]+" more than medium length, it length="+itemArray[i].length());
+        System.out.println("Medium length=" + mediumLength);
+        for (int i = 0; i < itemNumber; i++){
+            if (itemArray[i].length() > mediumLength)
+                System.out.println("Number " + itemArray[i]+" more than medium length, it length=" + itemArray[i].length());
         }
     }
 }
