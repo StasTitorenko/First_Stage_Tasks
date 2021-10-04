40cbb7511a0b52cd83832930bd7facb66ed1fd3f
Stas Titorenko <fanta2000@tut.by>
2021-10-04 09:13:10 +0300

    edit nextLine to toString

--- Java/javaTasks/src/main/java/javaFundamentials/mainTasks/firstTask/FirstTask.java
+++ Java/javaTasks/src/main/java/javaFundamentials/mainTasks/firstTask/FirstTask.java
@@ -9,7 +9,7 @@ public class FirstTask {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter Your name pls:");
-        String personName = input.nextLine();
+        String personName = input.toString();
         System.out.println(personName + " You are welcome!!!");
         input.close();
     }
