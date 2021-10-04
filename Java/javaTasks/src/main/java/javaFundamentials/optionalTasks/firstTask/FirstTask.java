0ae305e8e8ebda473f8852cefe6a169e4bae3c67
Stas Titorenko <fanta2000@tut.by>
2021-10-04 10:50:02 +0300

    change string array to int array

--- Java/javaTasks/src/main/java/javaFundamentials/optionalTasks/firstTask/FirstTask.java
+++ Java/javaTasks/src/main/java/javaFundamentials/optionalTasks/firstTask/FirstTask.java
@@ -11,28 +11,28 @@ public class FirstTask {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter the number of items you want to enter: ");
         int itemNumber = input.nextInt();
-        String[] itemArray = new String[itemNumber];
-        String shortestNumber = "";
-        String longestNumber = "";
+        int [] itemArray = new int [itemNumber];
+        int shortestNumber = 0;
+        int longestNumber = 0 ;
         int shortestNumberLength = 2147483647;
         int longestNumberLength = 0;
         for (int i = 0; i < itemNumber; i++) {
             System.out.print("Enter " + (i + 1) + " item:");
-            itemArray[i] = input.next();
-            if (itemArray[i].length() >= longestNumberLength) {
+            itemArray[i] = input.nextInt();
+            if (String.valueOf(itemArray[i]).length() >= longestNumberLength) {
                 longestNumber = itemArray[i];
-                longestNumberLength = itemArray[i].length();
+                longestNumberLength = String.valueOf(itemArray[i]).length();
             }
-            else if (itemArray[i].length() < shortestNumberLength) {
+            else if (String.valueOf(itemArray[i]).length() < shortestNumberLength) {
                 shortestNumber = itemArray[i];
-                shortestNumberLength = itemArray[i].length();
+                shortestNumberLength = String.valueOf(itemArray[i]).length();
             }
         }
         if (shortestNumberLength == 2147483647){ //Check no one else if
             shortestNumber = itemArray[0];
-            shortestNumberLength = itemArray[0].length();
+            shortestNumberLength = String.valueOf(itemArray[0]).length();
         }
-        System.out.println("Shortest number="+shortestNumber+" it's length="+shortestNumberLength);
-        System.out.println("Longest number="+longestNumber+" it's length="+longestNumberLength);
+        System.out.println("Shortest number=" + shortestNumber +" it's length=" + shortestNumberLength);
+        System.out.println("Longest number=" + longestNumber +" it's length=" + longestNumberLength);
     }
 }
