3124bd734deedc08481e38dfc087e6a1f2dad04d
Stas Titorenko <fanta2000@tut.by>
2021-10-04 11:06:05 +0300

    change string array to int array

--- Java/javaTasks/src/main/java/javaFundamentials/optionalTasks/thirdTask/ThirdTask.java
+++ Java/javaTasks/src/main/java/javaFundamentials/optionalTasks/thirdTask/ThirdTask.java
@@ -11,19 +11,19 @@ public class ThirdTask {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter the number of items you want to enter: ");
         int itemNumber = input.nextInt();
-        String[] itemArray = new String[itemNumber];
+        int [] itemArray = new int [itemNumber];
         int allLength = 0;
         for (int i = 0; i < itemNumber; i++) {
             System.out.print("Enter " + (i + 1) + " item:");
-            itemArray[i] = input.next();
-            allLength += itemArray[i].length();
+            itemArray[i] = input.nextInt();
+            allLength += String.valueOf(itemArray[i]).length();
         }
         input.close();
-        int mediumLength = allLength/itemNumber;
+        int mediumLength = allLength / itemNumber;
         System.out.println("Medium length=" + mediumLength);
         for (int i = 0; i < itemNumber; i++){
-            if (itemArray[i].length() > mediumLength)
-                System.out.println("Number " + itemArray[i]+" more than medium length, it length=" + itemArray[i].length());
+            if (String.valueOf(itemArray[i]).length() > mediumLength)
+                System.out.println("Number " + itemArray[i] + " more than medium length, it length=" + String.valueOf(itemArray[i]).length());
         }
     }
 }
