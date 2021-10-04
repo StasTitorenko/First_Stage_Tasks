89b29e4bdf135c33da4b7a7efd5e117ce39e0d92
Stas Titorenko <fanta2000@tut.by>
2021-10-04 09:00:41 +0300

    1.add "space" between operators

--- Java/javaTasks/src/main/java/javaFundamentials/optionalTasks/firstTask/FirstTask.java
+++ Java/javaTasks/src/main/java/javaFundamentials/optionalTasks/firstTask/FirstTask.java
@@ -12,25 +12,25 @@ public class FirstTask {
         System.out.print("Enter the number of items you want to enter: ");
         int itemNumber = input.nextInt();
         String[] itemArray = new String[itemNumber];
-        String shortestNumber="";
-        String longestNumber="";
-        int shortestNumberLength=2147483647;
-        int longestNumberLength=0;
-        for (int i=0;i<itemNumber;i++) {
-            System.out.print("Enter "+(i+1)+" item:");
-            itemArray[i]=input.next();
-            if (itemArray[i].length()>=longestNumberLength) {
-                longestNumber=itemArray[i];
-                longestNumberLength=itemArray[i].length();
+        String shortestNumber = "";
+        String longestNumber = "";
+        int shortestNumberLength = 2147483647;
+        int longestNumberLength = 0;
+        for (int i = 0; i < itemNumber; i++) {
+            System.out.print("Enter " + (i + 1) + " item:");
+            itemArray[i] = input.next();
+            if (itemArray[i].length() >= longestNumberLength) {
+                longestNumber = itemArray[i];
+                longestNumberLength = itemArray[i].length();
             }
-            else if (itemArray[i].length()<shortestNumberLength) {
-                shortestNumber=itemArray[i];
-                shortestNumberLength=itemArray[i].length();
+            else if (itemArray[i].length() < shortestNumberLength) {
+                shortestNumber = itemArray[i];
+                shortestNumberLength = itemArray[i].length();
             }
         }
-        if (shortestNumberLength==2147483647){ //Check no one else if
-            shortestNumber=itemArray[0];
-            shortestNumberLength=itemArray[0].length();
+        if (shortestNumberLength == 2147483647){ //Check no one else if
+            shortestNumber = itemArray[0];
+            shortestNumberLength = itemArray[0].length();
         }
         System.out.println("Shortest number="+shortestNumber+" it's length="+shortestNumberLength);
         System.out.println("Longest number="+longestNumber+" it's length="+longestNumberLength);
